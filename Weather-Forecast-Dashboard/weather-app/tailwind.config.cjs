0d6aee9228a38d04bb41fd001c3e02f1a5e09260
BethelHills <bethelhillary@gmail.com>
2025-12-17 05:22:04 -0800

    Fix Vercel deployment configuration

--- Weather-Forecast-Dashboard/weather-app/tailwind.config.cjs
+++ Weather-Forecast-Dashboard/weather-app/tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkStart: "#002E78",
+        darkEnd: "#160524",
+        lightStart: "#6E85D3",
+        lightEnd: "#C38EF0",
+      },
+    },
   },
   plugins: [],
 }
